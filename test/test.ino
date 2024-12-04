4bfeed3692845ebeff9fd0e5687535c110c8dad9
Corin B <corin.barker@education.nsw.gov.au>
2024-12-05 09:44:11 +1100

    happy days

--- test/test.ino
+++ test/test.ino
@@ -1,19 +1,19 @@
-/*
-  Purpose: Basic example of the Seead Buzzer output module
-  Notes: 
-    1. Connect to a PWN Digital pin
-    2. Songs https://github.com/robsoncouto/arduino-songs
-  Author: Ben Jones 13/7/23
-  Contact: benjmain.jones21@det.nsw.edu.au
-  Source: https://www.seeedstudio.com/Grove-Buzzer.html
-*/
+#include "WT2605C_Player.h"
 
+#ifdef __AVR__
+  #include <SoftwareSerial.h>
+  SoftwareSerial SSerial(2, 3); // RX, TX
+  #define COMSerial SSerial
+  #define ShowSerial Serial
 
+  WT2605C<SoftwareSerial> Mp3Player;
+#endif
 
-void setup () {
+void setup() {
+  while (!ShowSerial);
+  ShowSerial.begin(9600);
+  COMSerial.begin(115200);
 }
-
-void loop () {
-
-
+void loop() {
+Mp3Player.playSDSong(fileName)
 }
