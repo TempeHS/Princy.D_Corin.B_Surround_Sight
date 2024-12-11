70f7b8d7e2b123832831bfc782f03cc7e1896595
Corin B <corin.barker@education.nsw.gov.au>
2024-12-11 14:52:49 +1100

    no

--- Surround.Sight/Surround.Sight.ino
+++ Surround.Sight/Surround.Sight.ino
@@ -9,14 +9,16 @@
 // x1 vibration sensor
 // x1 ultrasonic ranger
 #include "Ultrasonic.h"
-
+#define SPEAKER 3
 Ultrasonic myUltrasonicSensor(6);
 int OPDevice = 7;
+int BassTab[]={1911,1702,1516,1431,1275,1136,1012};//bass 1~7
 
 void setup()
 {
  digitalWrite(OPDevice, HIGH);
  Serial.begin(9600);
+ pinInit();
 }
 void loop()
 {
@@ -31,4 +33,21 @@ void loop()
   } else {
     digitalWrite(OPDevice, LOW);
   }
+   /*sound bass 1~7*/
+    for(int note_index=0;note_index<7;note_index++)
+    {
+        sound(note_index);
+    }
+}
+void pinInit()
+{
+    pinMode(SPEAKER,OUTPUT);
+    digitalWrite(SPEAKER,HIGH);
+}
+void sound(uint8_t note_index)
+{
+    for(int i=0;i<100;i++)
+    {
+       
+    }
 }
